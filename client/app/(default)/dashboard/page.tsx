8a50fbdf9b03071440e96fb7a5114d450c909491
Prashanth BR <prashanthrepo@gmail.com>
2023-12-02 15:51:48 +0530

    map changes

--- client/app/(default)/dashboard/page.tsx
+++ client/app/(default)/dashboard/page.tsx
@@ -16,7 +16,7 @@ export default function Dashboard() {
   const dashboardRender = (type) => {
     switch (type) {
       case 'Buyer':
-        return <MapView />;
+        return <></>;
       case 'Seller':
         return <SellerLayouts />;
       case 'Admin':
