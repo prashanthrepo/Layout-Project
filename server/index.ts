2828c4d681c5436bee574258059202ba1afc2d63
Sachin Tom <sachintomacc@gmail.com>
2023-12-14 15:19:06 +0530

    sample BE Change

--- server/index.ts
+++ server/index.ts
@@ -32,7 +32,7 @@ mongoose
     .connect(process.env.MONGO_URI as string, {})
     .then(() => {
         app.listen(port, () => {
-            console.log(`connected to DB, listening on ${port} ✅`)
+            console.log(`connected to DB, listening on ${port} ....`)
         })
     })
     .catch(error => {
