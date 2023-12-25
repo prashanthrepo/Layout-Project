662bd6225714d6af010985c85bf0b9d470fa288c
Sachin Tom <sachintomacc@gmail.com>
2023-12-25 09:31:19 +0530

    create transaction controller

---
+++ server/controllers/transactionController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import Transaction from "../models/transaction"
+import { transactionSchema } from "../zod/schemas"
+
+const createTransaction = async (req: Request, res: Response) => {
+    const parsedData = transactionSchema.safeParse(req.body)
+    // console.log("123 ===",123)
+    // console.log("parsedData ===",parsedData)
+
+    if (!parsedData.success) {
+        return res.status(500).json(parsedData.error)
+
+        // console.log("parsedData.data ===",parsedData.data)
+    } else {
+        const txn = await Transaction.create({ ...parsedData.data })
+        await txn.save()
+        // console.log({...parsedData.data})
+        return res.status(200).json(txn)
+    }
+}
+
+
+export { createTransaction }
