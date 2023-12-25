f276fbe5f9162df29f0f482cc05ba9cde7a9e748
Sachin Tom <sachintomacc@gmail.com>
2023-12-25 09:30:48 +0530

    adding transaction routes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,15 @@
+import { router as baseRoutes } from "./baseRoutes"
 import { router as layoutRoutes } from "./layoutRoutes"
 import { router as leadRoutes } from "./leadRoutes"
 import { router as siteRoutes } from "./siteRoutes"
 import { router as tokenRoutes } from "./tokenRoutes"
-import { router as baseRoutes } from "./baseRoutes"
+import { router as transactionRoutes } from "./transactionRoutes"
 
-export { layoutRoutes, leadRoutes, siteRoutes,tokenRoutes,baseRoutes }
+export {
+    baseRoutes,
+    layoutRoutes,
+    leadRoutes,
+    siteRoutes,
+    tokenRoutes,
+    transactionRoutes,
+}
