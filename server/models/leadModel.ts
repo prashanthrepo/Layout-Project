25ca495185e52afa380c5f0b9f673215c14e0e3b
Sachin Tom <sachintomacc@gmail.com>
2024-01-01 14:01:07 +0530

    add created date to Lead model

--- server/models/leadModel.ts
+++ server/models/leadModel.ts
@@ -11,15 +11,18 @@ export interface LeadDocument extends Document {
     status?: string
 }
 
-const leadSchema = new Schema<LeadDocument>({
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: false },
-    buyerOffer: { type: Number, required: false },
-    sellerOffer: { type: Number, required: false },
-    finalPrice: { type: Number, required: false },
-    notes: { type: String, required: false },
-    status: { type: String, required: false },
-})
+const leadSchema = new Schema<LeadDocument>(
+    {
+        name: { type: String, required: true },
+        phone: { type: String, required: true },
+        email: { type: String, required: false },
+        buyerOffer: { type: Number, required: false },
+        sellerOffer: { type: Number, required: false },
+        finalPrice: { type: Number, required: false },
+        notes: { type: String, required: false },
+        status: { type: String, required: false },
+    },
+    { timestamps: true }
+)
 
 export default mongoose.model("Lead", leadSchema)
