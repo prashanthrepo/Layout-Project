96504f2025300f49bc8fef142f0b25f9c6692431
Sachin Tom <sachintomacc@gmail.com>
2024-01-14 11:11:16 +0530

    updated Controller

--- server/controllers/layoutController.ts
+++ server/controllers/layoutController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 import mongoose from "mongoose"
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OBJECT_NOT_FOUND } from "../config"
 import { default as Layout } from "../models/layoutModel"
 import siteModel, { Site } from "../models/siteModel"
 import { layoutSchema } from "../zod/schemas"
@@ -9,7 +10,7 @@ const createLayout = async (req: Request, res: Response) => {
         const parsedData = layoutSchema.safeParse(req.body)
 
         if (!parsedData.success) {
-            res.sendError({ details: parsedData.error.issues }, 400, "Bad Request",)
+            res.sendError(BAD_REQUEST, { details: parsedData.error.issues })
         }
         else {
             const layout = await Layout.create({ ...parsedData.data, })
@@ -26,7 +27,7 @@ const createLayout = async (req: Request, res: Response) => {
 
         }
     } catch (error) {
-        res.sendError({ error })
+        res.sendError(INTERNAL_SERVER_ERROR, { error })
     }
 }
 
@@ -44,31 +45,27 @@ const getSingleLayout = async (req: Request, res: Response) => {
     try {
         if (!mongoose.Types.ObjectId.isValid(id)) {
 
-            res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+            res.sendError(OBJECT_NOT_FOUND)
             return
         }
         const layout = await Layout.findOne({ _id: id }).populate("sites")
-        !layout && res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+        !layout && res.sendError(OBJECT_NOT_FOUND)
         layout && res.sendSuccess(layout)
 
     } catch (error) {
-        res.sendError({ error })
+        res.sendError(INTERNAL_SERVER_ERROR, { error })
     }
 
-
-
 }
-
-
 
 const deleteLayout = async (req: Request, res: Response) => {
     const { id } = req.params
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+        res.sendError(OBJECT_NOT_FOUND)
         return
     }
     const layout = await Layout.findOneAndDelete({ _id: id })
-    !layout && res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+    !layout && res.sendError(OBJECT_NOT_FOUND)
     res.sendSuccess({}, 200)
 
 }
@@ -77,7 +74,7 @@ const updateLayout = async (req: Request, res: Response) => {
     const { id } = req.params
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+        res.sendError(OBJECT_NOT_FOUND)
         return
     }
 
@@ -89,11 +86,11 @@ const updateLayout = async (req: Request, res: Response) => {
         )
         await layout?.save()
         if (!layout) {
-            res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+            res.sendError(OBJECT_NOT_FOUND)
         }
         res.sendSuccess(layout)
     } catch (error) {
-        res.sendError({ error })
+        res.sendError(INTERNAL_SERVER_ERROR, { error })
     }
 
 }
@@ -102,7 +99,7 @@ const getLayoutLeads = async (req: Request, res: Response) => {
     const { id } = req.params
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-        res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+        res.sendError(OBJECT_NOT_FOUND)
         return
     }
 
@@ -114,43 +111,15 @@ const getLayoutLeads = async (req: Request, res: Response) => {
         const leads = layout.leads
         res.sendSuccess(leads)
     } else {
-        res.sendError({ error: "Layout not found" }, 404, "Layout not found")
+        res.sendError(OBJECT_NOT_FOUND)
     }
 }
 
-// const getAllLeads = async (req: Request, res: Response) => {
-//     const { id } = req.params
 
-//     if (!Types.ObjectId.isValid(id)) {
-//     }
-//     const layout = await layoutModel
-//         .findById(id)
-//         .populate({
-//             path: "sites",
-//             populate: {
-//                 path: "leads",
-//                 model: "Lead",
-//             },
-//         })
-//         .populate("leads")
-
-//     let allLeads = []
-
-//     if (layout) {
-//         layout.sites.forEach(site => {
-//             allLeads = [...allLeads, ...site.leads]
-//         })
-
-//         allLeads = [...allLeads, ...layout?.leads]
-//     }
-
-//     return res.status(200).json({ allLeads })
-// }
 
 export {
     createLayout,
     deleteLayout,
-    // getAllLeads,
     getLayoutLeads,
     getLayouts,
     getSingleLayout,
