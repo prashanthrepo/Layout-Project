40440fb88c4ee2e67789807db5ad36ac2248d437
Sachin Tom <sachintomacc@gmail.com>
2024-01-15 20:44:51 +0530

    added status to token model

--- server/models/token.ts
+++ server/models/token.ts
@@ -7,7 +7,7 @@ interface Token {
     lead: Types.ObjectId | Layout
     tokenAmount: number
     expiryDate: Date
-    status: string
+    status: "active" | "inactive"
 }
 
 const tokenSchema = new Schema<Token & Document>(
@@ -16,7 +16,7 @@ const tokenSchema = new Schema<Token & Document>(
         lead: { type: Types.ObjectId, ref: "Lead", required: true },
         tokenAmount: { type: Number, required: true },
         expiryDate: { type: Date, required: true },
-        status: { type: String, required: true, default: "active" },
+        status: { type: String, required: true, enum: ['active', 'inactive'], default: "active" },
     },
     { timestamps: true }
 )
