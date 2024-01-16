bfc778e7301c62d85f5b10c7cdd2e81e5b5b6010
Prashanth BR <prashanthrepo@gmail.com>
2024-01-16 15:49:15 +0530

    changes

--- client/app/(default)/property/[slug]/site/site-details/details.tsx
+++ client/app/(default)/property/[slug]/site/site-details/details.tsx
@@ -54,8 +54,7 @@ export default function SiteDetails({ siteDetails, setOpenCancelToken }) {
       )}
       {siteDetails?.status == 'Token' && (
         <div className="rounded-md bg-blue-50 p-4">
-          {cancelToken ? (
-            <div className=" inset-0 z-10 overflow-y-auto">
+          {/* <div className=" inset-0 z-10 overflow-y-auto">
               <div className="flex items-end justify-center sm:p-4 text-center sm:items-center sm:px-24">
                 <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all  w-full sm:p-6">
                   <div className="flex items-start">
@@ -100,78 +99,74 @@ export default function SiteDetails({ siteDetails, setOpenCancelToken }) {
                   </div>
                 </div>
               </div>
+            </div> */}
+          <div className="flex">
+            <div className="flex-shrink-0">
+              <svg
+                className="h-5 w-5 text-blue-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true">
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
+                  clipRule="evenodd"></path>
+              </svg>
             </div>
-          ) : (
-            <div className="flex">
-              <div className="flex-shrink-0">
-                <svg
-                  className="h-5 w-5 text-blue-400"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true">
-                  <path
-                    fillRule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
-                    clipRule="evenodd"></path>
-                </svg>
-              </div>
-              <div className="ml-3 text-slate-600">
-                <h3 className=" font-medium text-blue-700">
-                  Token expires in{' '}
-                  {daysBetween(siteDetails?.statusMetadata?.token?.expiryDate)}
-                  days
-                </h3>
+            <div className="ml-3 text-slate-600">
+              <h3 className=" font-medium text-blue-700">
+                Token expires in{' '}
+                {daysBetween(siteDetails?.statusMetadata?.token?.expiryDate)}
+                days
+              </h3>
 
-                <p>
-                  Token to{' '}
-                  <span className="font-semibold">
-                    {siteDetails?.statusMetadata?.token?.lead?.name}
-                  </span>
-                </p>
-                <p>
-                  Token amount{' '}
-                  <span className="font-semibold">
-                    {siteDetails?.statusMetadata?.token?.tokenAmount}
-                  </span>
-                </p>
-                <p>
-                  Token given on{' '}
-                  <span className="font-semibold">
-                    {convertDate(siteDetails?.statusMetadata?.token?.createdAt)}
-                  </span>
-                </p>
-                <p>
-                  Token expires on{' '}
-                  <span className="font-semibold">
-                    {convertDate(
-                      siteDetails?.statusMetadata?.token?.expiryDate
-                    )}
-                  </span>
-                </p>
-                <p>
-                  Notes{' '}
-                  <span className="font-semibold">
-                    {siteDetails?.statusMetadata?.notes}
-                  </span>
-                </p>
-                <div className="mt-1">
-                  <div className="my-1.5 space-x-2 flex">
-                    <button
-                      onClick={() => setCancelToken(true)}
-                      type="button"
-                      className="rounded-md bg-red-100 px-2 py-1.5 text-sm font-medium text-red-800 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50">
-                      Cancel token
-                    </button>
-                    <button
-                      type="button"
-                      className="rounded-md bg-blue-200 px-2 py-1.5 text-sm font-medium text-blue-800 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-blue-50">
-                      Mark as sold
-                    </button>
-                  </div>
+              <p>
+                Token to{' '}
+                <span className="font-semibold">
+                  {siteDetails?.statusMetadata?.token?.lead?.name}
+                </span>
+              </p>
+              <p>
+                Token amount{' '}
+                <span className="font-semibold">
+                  {siteDetails?.statusMetadata?.token?.tokenAmount}
+                </span>
+              </p>
+              <p>
+                Token given on{' '}
+                <span className="font-semibold">
+                  {convertDate(siteDetails?.statusMetadata?.token?.createdAt)}
+                </span>
+              </p>
+              <p>
+                Token expires on{' '}
+                <span className="font-semibold">
+                  {convertDate(siteDetails?.statusMetadata?.token?.expiryDate)}
+                </span>
+              </p>
+              <p>
+                Notes{' '}
+                <span className="font-semibold">
+                  {siteDetails?.statusMetadata?.notes}
+                </span>
+              </p>
+              <div className="mt-1">
+                <div className="my-1.5 space-x-2 flex">
+                  <button
+                    onClick={() => setOpenCancelToken(true)}
+                    type="button"
+                    className="rounded-md bg-red-100 px-2 py-1.5 text-sm font-medium text-red-800 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 focus:ring-offset-red-50">
+                    Cancel token
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md bg-blue-200 px-2 py-1.5 text-sm font-medium text-blue-800 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-blue-50">
+                    Mark as sold
+                  </button>
                 </div>
               </div>
             </div>
-          )}
+          </div>
         </div>
       )}
       <ul className="space-y-2 my-2">
