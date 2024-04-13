b2f040ed0fe5490cf5231daecfc37ed67d958a52
Sachin Tom <62328681+sachintom999@users.noreply.github.com>
2024-04-13 08:23:43 +0530

    feat: approval edit and delete (#169)

--- server/controllers/approvalController.ts
+++ server/controllers/approvalController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 
 import { layoutSchema } from "../zod/schemas"
 import { ApprovalModel, LayoutApprovalModel } from "../models/approvalModel"
+import { OBJECT_NOT_FOUND, SOMETHING_WENT_WRONG } from "../config"
 
 const getApprovalList = async(req: Request, res: Response) => {
 
@@ -68,4 +69,37 @@ const updateApproval = async(req: Request, res: Response) => {
 
 
 
-export { getApprovalList, createApproval,updateApproval,createApprovalMapping}
+const deleteApproval = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const lead = await ApprovalModel.findOneAndDelete({ _id: id })
+        lead && res.sendSuccess({}, 200)
+        !lead && res.sendError(OBJECT_NOT_FOUND)
+    } catch (error) {
+        res.sendError(SOMETHING_WENT_WRONG, { error })
+    }
+}
+
+
+
+const editApproval = async (req: Request, res: Response) => {
+    const { id } = req.params
+    try {
+        const approval = await ApprovalModel.findOneAndUpdate(
+            { _id: id },
+            { ...req.body },
+            { new: true }
+        )
+        await approval?.save()
+        !approval && res.sendError(OBJECT_NOT_FOUND)
+        approval && res.sendSuccess(approval)
+
+    } catch (error) {
+        res.sendError(SOMETHING_WENT_WRONG, { error })
+    }
+
+
+}
+
+
+export { getApprovalList, createApproval,updateApproval,createApprovalMapping,editApproval,deleteApproval}
--- server/routes/approvalRoutes.ts
+++ server/routes/approvalRoutes.ts
@@ -1,17 +1,21 @@
-import { Router } from "express"
+import { Router } from "express";
 
+import { checkAuth, isAdmin, validateId } from "../middlewares";
+import {
+  createApproval,
+  createApprovalMapping,
+  deleteApproval,
+  editApproval,
+  getApprovalList,
+  updateApproval,
+} from "../controllers/approvalController";
 
-import { checkAuth, isAdmin, validateId } from "../middlewares"
-import { createApproval, createApprovalMapping, getApprovalList, updateApproval } from "../controllers/approvalController"
+export const router = Router();
 
-export const router = Router()
+// router.post("/select-approvals", checkAuth, createApprovalMapping)
+// router.patch("/", checkAuth, updateApproval)
 
-
-
-
-router.get("/", checkAuth, getApprovalList)
-router.post("/", checkAuth, isAdmin, createApproval)
-router.post("/select-approvals", checkAuth, createApprovalMapping)
-router.patch("/", checkAuth, updateApproval)
-
-
+router.get("/", checkAuth,  getApprovalList);
+router.post("/", checkAuth, isAdmin, createApproval);
+router.patch("/:id", checkAuth, isAdmin, editApproval);
+router.delete("/:id", checkAuth, isAdmin, deleteApproval);
